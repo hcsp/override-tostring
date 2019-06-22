d67fa50975bd6be34a3d445bde2d01f51ddc96e7
Bo Zhang <bo@gradle.com>
2019-06-22 19:10:15 +0800

    Sync repositories

---
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -0,0 +1,26 @@
+package com.github.hcsp.inheritance;
+
+public class User {
+    private Integer id;
+    private String name;
+
+    public User(Integer id, String name) {
+        this.id = this.id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new User(1, "user1"));
+    }
+
+    // 请在这里覆盖equals方法，使得在打印用户时能将用户的id和name打印出来
+
+}
---
+++ src/test/java/com/github/hcsp/inheritance/UserTest.java
@@ -0,0 +1,24 @@
+package com.github.hcsp.inheritance;
+
+import com.github.hcsp.test.helper.ProjectSourceFileReader;
+import java.util.Random;
+import java.util.UUID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+    @Test
+    public void test() {
+        int id = new Random().nextInt(1000);
+        String name = UUID.randomUUID().toString();
+        String userString = new User(id, name).toString();
+        Assertions.assertTrue(userString.contains(id));
+        Assertions.assertTrue(userString.contains(name));
+    }
+
+    @Test
+    public void hasOverride() {
+        Assertions.assertTrue(
+                ProjectSourceFileReader.readAsString(User.class).contains("@Override"));
+    }
+}
