ecb118914e7bf7cd6b886c586dfbcb5d50a251cc
zyw <40329182+zhangyongwang4444@users.noreply.github.com>
2019-06-25 09:21:20 +0800

    覆盖toString方法 (#4)
    覆盖toString方法 (#4)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.inheritance;
 
+import java.text.MessageFormat;
+
 public class User {
     private Integer id;
     private String name;
@@ -23,4 +25,9 @@ public class User {
 
     // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
 
+    @Override
+
+    public String toString() {
+        return MessageFormat.format("id: {0}, name: {1}", id, name);
+    }
 }
