bc6d7374a706bc0b520a7805e4f25cb607b521c4
sunp <sunp@SH-CF12-DEV.rnd.aspentech.com>
2019-12-12 01:07:40 -0800

    Java面向对象初步练习：覆盖toString方法

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,26 +1,33 @@
 package com.github.hcsp.inheritance;
 
 public class User {
-    private Integer id;
-    private String name;
+	private Integer id;
+	private String name;
 
-    public User(Integer id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+	public User(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public static void main(String[] args) {
-        System.out.println(new User(1, "user1"));
-    }
+	public static void main(String[] args) {
+		System.out.println(new User(1, "user1"));
+	}
 
-    // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
+	// 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
+
+	@Override
+	public String toString() {
+
+		return new User(id, name).getId() + "" + new User(id, name).getName();
+
+	}
 
 }
