a2accb12e3b60038137a19a89ac87896cfb683e0
= <>
2020-01-05 11:29:17 +0800

    Java面向对象初步练习：覆盖toString方法

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -18,9 +18,12 @@ public class User {
     }
 
     public static void main(String[] args) {
-        System.out.println(new User(1, "user1"));
+        System.out.println(new User(1, "user1").toString());
     }
 
     // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
-
+    @Override
+    public String toString() {
+        return id + name;
+    }
 }
