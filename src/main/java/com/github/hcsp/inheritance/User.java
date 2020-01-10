28f9af42701c4df4158ec921be85805866fb6067
fiymi <fiymi666@gmail.com>
2020-01-11 04:30:34 +0800

    覆盖toString

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -19,8 +19,15 @@ public class User {
 
     public static void main(String[] args) {
         System.out.println(new User(1, "user1"));
+
     }
 
+
     // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
+    @Override
+    public String toString(){
+        return(getId()+getName());
+    }
+
 
 }
