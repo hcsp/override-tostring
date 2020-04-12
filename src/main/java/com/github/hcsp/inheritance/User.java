22efbb820cecfeb46a5e70f07e0f2790fcf03a34
chenz <czj2140811@gmail.com>
2020-04-12 08:11:00 -0700

    tostring

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.inheritance;
 
-public class User {
+public class User extends Object {
     private Integer id;
     private String name;
 
@@ -22,5 +22,9 @@ public class User {
     }
 
     // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
+    @Override
+    public String toString(){
+        return "User{" + "id = " + id +", name = '" + name +'\'' + '}';
+    }
 
 }
