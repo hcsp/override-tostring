57cf87e6b2f790a9b5b7ff5e9a66013ea166d93b
zhu <z823511280>
2020-09-10 00:07:12 +0800

    toStrings

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -22,8 +22,10 @@ public class User {
     }
 
     // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
-
-    public String toString(){
-        return "{id="+id+"   name="+name+"}";
+    @Override
+    public String toString() {
+        return "{id=" + id + ",name=" + name + "}";
     }
+
+
 }
