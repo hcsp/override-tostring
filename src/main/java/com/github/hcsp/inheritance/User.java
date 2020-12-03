55e1a90291ee47739dba9a8182e3165debd9331a
weiranyi <168578203@qq.com>
2020-12-03 19:32:45 +0800

    将+号放在了独立行

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -25,9 +25,18 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
+        return "User{"
+                +
+                "id="
+                +
+                id
+                +
+                ", name='"
+                +
+                name
+                +
+                '\''
+                +
                 '}';
     }
 }
