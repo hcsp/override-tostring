880544710888fd3105a12e128c82a61b7609dd2c
yang song <1479867360@qq.com>
2020-12-11 13:22:36 +0800

    覆盖toString方法 (#197)
    覆盖toString方法 (#197)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -4,9 +4,18 @@ public class User {
     private Integer id;
     private String name;
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
     public User(Integer id, String name) {
         this.id = id;
         this.name = name;
+
     }
 
     public Integer getId() {
