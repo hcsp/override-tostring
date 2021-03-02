3dcb4008f385c6a28abaf04063db6b0663a167a6
momoko <1165874815@qq.com>
2021-03-02 16:40:26 +0800

    override toString

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -25,9 +25,12 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return "User{"
+                + "id="
+                + id
+                + ", name='"
+                + name
+                + '\''
+                + '}';
     }
 }
