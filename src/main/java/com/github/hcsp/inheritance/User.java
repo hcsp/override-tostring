a57fd1f32e096f702318ec6fa22c8b354db2c4bc
yangbisheng <2606424906@qq.com>
2022-02-01 02:51:49 +0800

    override toString method (#237)
    override toString method (#237)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,6 +1,7 @@
 package com.github.hcsp.inheritance;
 
-public class User {
+public class
+User {
     private Integer id;
     private String name;
 
@@ -22,5 +23,8 @@ public class User {
     }
 
     // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
-
+    @Override
+    public String toString() {
+        return "name:" + getName() + " id:" + getId();
+    }
 }
