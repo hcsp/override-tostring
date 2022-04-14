a3ffa958866d96824b6adfc415749d51ea29dd92
Ryan <1572382023@qq.com>
2022-04-14 23:53:32 +0800

    我已完成覆盖toString方法的作业

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,8 +1,8 @@
 package com.github.hcsp.inheritance;
 
 public class User {
-    private Integer id;
-    private String name;
+    private final Integer id;
+    private final String name;
 
     public User(Integer id, String name) {
         this.id = id;
@@ -23,4 +23,11 @@ public class User {
 
     // 请在这里覆盖toString方法，使得在打印用户时能将用户的id和name打印出来
 
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
