6a3342fa7a6bf0c6471874b0b218c2d7f2e875e8
Ryan <1572382023@qq.com>
2022-04-16 13:07:00 +0800

    我已完成覆盖toString方法的作业1

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -19,9 +19,8 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{"
-                + "id=" + id
-                + ", name='" + name + '\''
-                + '}';
+        return "User{id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 }
